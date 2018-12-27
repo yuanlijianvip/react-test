99655bd7f959a2187847d85d22256f6b2afc6f57
yuanlijianvip <yuanlijianvip@163.com>
2018-12-27 18:07:17 +0800

    todolist

---
+++ todolist1/src/TodoList.js
@@ -0,0 +1,36 @@
+import React,{ Component,Fragment } from 'react';
+
+class TodoList extends Component{
+
+	constructor(props){
+		super(props);
+		this.state={
+				inputValue:'',
+				list:[]
+		}
+}
+
+	render(){
+		return(
+			<Fragment>
+				<div>
+					<input 
+						value={this.state.inputValue}
+						onChange={this.handleInputChange}
+					/>
+					<button>提交</button>
+				</div>
+				<ul>
+					<li>学英语</li>
+					<li>Learning React</li>
+				</ul>
+			</Fragment>
+		)
+		}
+
+		handleInputChange(e){
+			this.state.inputValue=e.target.value;
+		}
+	}
+
+export default TodoList;
