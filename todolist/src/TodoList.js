be37985f6f86f29ed34f9ae557a0d156519b07b4
yuanlijianvip <yuanlijianvip@163.com>
2018-12-27 15:37:12 +0800

    todolist

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,98 @@
+import React,{ Component,Fragment } from 'react';
+import TodoItem from './TodoItem';
+import './style.css'
+
+class TodoList extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            inputValue:'',
+            list:[]
+        }
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemDelete=this.handleItemDelete.bind(this);
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <div>
+                    <label htmlFor="insertArea">输入内容</label>
+                    {/*下面是一个input框 */}
+                    {
+                        //下面是一个input框
+                    }
+                    <input 
+                        id="insertArea"
+                        className='input'
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange}
+                        />
+                    <button onClick={this.handleBtnClick}>提交</button>
+                </div>
+                <ul>
+                    {this.getTodoItem()}
+                </ul>
+            </Fragment>
+        )
+    }
+
+    getTodoItem(){
+       return this.state.list.map((item,index)=>{
+            return (
+                <div>
+                <TodoItem 
+                    content={item} 
+                    index={index}
+                    deleteItem={this.handleItemDelete}
+                    />
+                {/*<li 
+                    key={index} 
+                    onClick={this.handleItemDelete.bind(this,index)}
+                    dangerouslySetInnerHTML={{__html:item}}
+                >
+               
+                </li>*/}
+                </div>
+            ) 
+        })
+    }
+
+    handleInputChange(e){
+        const value=e.target.value;
+        this.setState(()=>({
+            inputValue:value
+        }));
+        // this.setState({
+        //     inputValue:e.target.value
+        // })
+    }
+ 
+    handleBtnClick(){
+        this.setState((prevState)=>({
+            list:[...prevState.list,prevState.inputValue],
+            inputValue:''
+        }));
+    }
+
+    handleItemDelete(index){
+        //immutable
+        //this.state.list.splice(index,1)
+        //state 不允许我们做任何的改变
+
+        // const list=[...this.state.list];
+        // list.splice(index,1);
+
+        // this.setState({
+        //     list:list
+        this.setState((prevState)=>{
+            const list=[...prevState.list];
+            list.splice(index,1);
+            return {list}
+        });
+    }
+}
+
+export default TodoList;
